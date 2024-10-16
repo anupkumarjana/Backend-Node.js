23a4dd62f48727be8b6b415197f0f95ccd69f551
Anup K Jana <janaanup7872@gmail.com>
2024-10-16 10:14:42 +0530

    making simple api

--- 2nd-Lecture/index.js
+++ 2nd-Lecture/index.js
@@ -1,7 +1,45 @@
-require('dotenv').config()
+require("dotenv").config();
 
 const express = require("express");
 const app = express();
+
+const gihubData = {
+  login: "anupkumarjana",
+  id: 91030891,
+  node_id: "MDQ6VXNlcjkxMDMwODkx",
+  avatar_url: "https://avatars.githubusercontent.com/u/91030891?v=4",
+  gravatar_id: "",
+  url: "https://api.github.com/users/anupkumarjana",
+  html_url: "https://github.com/anupkumarjana",
+  followers_url: "https://api.github.com/users/anupkumarjana/followers",
+  following_url:
+    "https://api.github.com/users/anupkumarjana/following{/other_user}",
+  gists_url: "https://api.github.com/users/anupkumarjana/gists{/gist_id}",
+  starred_url:
+    "https://api.github.com/users/anupkumarjana/starred{/owner}{/repo}",
+  subscriptions_url: "https://api.github.com/users/anupkumarjana/subscriptions",
+  organizations_url: "https://api.github.com/users/anupkumarjana/orgs",
+  repos_url: "https://api.github.com/users/anupkumarjana/repos",
+  events_url: "https://api.github.com/users/anupkumarjana/events{/privacy}",
+  received_events_url:
+    "https://api.github.com/users/anupkumarjana/received_events",
+  type: "User",
+  site_admin: false,
+  name: "Anup Kumar Jana",
+  company: null,
+  blog: "anupz.dev",
+  location: null,
+  email: null,
+  hireable: null,
+  bio: "Hi, I'm an SDE at MountBlue.",
+  twitter_username: null,
+  public_repos: 42,
+  public_gists: 1,
+  followers: 2,
+  following: 5,
+  created_at: "2021-09-19T19:49:01Z",
+  updated_at: "2024-09-16T06:34:04Z",
+};
 
 app.get("/", (req, res) => {
   try {
@@ -21,7 +59,17 @@ app.get("/twitter", (req, res) => {
 
 app.get("/login", (req, res) => {
   try {
-    res.send('<h1>Hi this is a login page<h1/>');
+    res.send("<h1>Hi this is a login page<h1/>");
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// making api
+
+app.get("/github-api", (req, res) => {
+  try {
+    res.json(gihubData);
   } catch (err) {
     console.log(err);
   }
